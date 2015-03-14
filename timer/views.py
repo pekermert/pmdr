5ffee7feb1b24210cf5bae83e2f66d42fe0490f3
pmoksuz <pekermert@email.com>
2015-03-14 03:38:11 +0200

    django class based routing added

--- timer/views.py
+++ timer/views.py
@@ -1,4 +1,4 @@
-from rest_framework.views import APIView
+from rest_framework.decorators import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
@@ -7,33 +7,77 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from timer.models import TimeRecord,User
 from timer.serializers import TimeRecordSerializer,UserSerializer
 
-class TodayList(APIView):
-	'''
-	List of todays pomodoros
-	'''
-	def get(self, request, format=None):
-		d = datetime.datetime.now()
-		datenow = "%s-%s-%s" % (d.year, d.month, d.day)
-		pomodoroList = TimeRecord.objects.filter(on_date=datenow)
-		serializer   = TimeRecordSerializer(pomodoroList, many=True)
-		return Response(serializer.data , status=status.HTTP_200_OK)
-
-class UserView(APIView):
-	'''authentication_classes = (JSONWebTokenAuthentication, )
+	
+class UserProfileView (APIView):
+	authentication_classes = (JSONWebTokenAuthentication, )
 	permission_classes = ()
-'''
-	def get(self, request, format=None):
-		'''data = {
-			'id': request.user.id,
-			'username': request.user.username,
-			'token': str(request.auth)
-		}
-		return Response(data)
-		'''
-		userList = User.objects.all()
-		serializer = UserSerializer(userList, many=True)
+
+	def get(self, request, user_id=None, *args, **kw):
+		#get_arg1 = request.GET.get('arg1', None)
+		if user_id is None:
+			user_id = request.user.id
+		user = User.objects.filter(id=user_id)
+		serializer = UserSerializer(user,many=True)
 		return Response(serializer.data, status=status.HTTP_200_OK)
+
+	#TODO : put method = Update profile
+
+class TimeRecordView (APIView):	
+	authentication_classes = (JSONWebTokenAuthentication, )
+	permission_classes = ()
+
+	def get(self, request, user_id=None, *args, **kw):
+		try:
+			if user_id is None:
+				user_id = request.user.id
+			last_record = TimeRecord.objects.filter(owner=user_id).latest('id')
+			serializer = TimeRecordSerializer(last_record,many=False)
+			#Check Latest Record validity
+			#Add modifier for ST SB LB
+			#Add modifier for status DN CT CL
+			return Response(serializer.data, status=status.HTTP_200_OK)
+		except TimeRecord.DoesNotExist:
+			return Response(status=status.HTTP_404_NOT_FOUND)
+
+	def post(self, request):
+		try:
+			new_data = TimeRecordSerializer(data = request.data)
+			if new_data.is_valid():
+				new_data.save()
+				return Response(new_data.data, status=status.HTTP_201_CREATED)
+			return Response(new_data.errors, status=status.HTTP_400_BAD_REQUEST)
+		except TimeRecord.DoesNotExist: # Error ?? ! 
+			return Response(new_data.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+	#TODO : put method = Update state timer DONE , RESET
+	def put(self, request, timer_id=None, *args, **kw):
+		try:
+			timer_status = request.GET.get('status', None)
+			if timer_id:
+				if timer_status:
+					# THERE IS NOT OK    !!!!!!!!!!!!
+					record = TimeRecord.objects.filter(id=timer_id,owner=request.user.id) \
+					.update(status=timer_status)
+			else:	
+				return Response(status=status.HTTP_400_BAD_REQUEST)
+		except:
+			return Response(status=status.HTTP_400_BAD_REQUEST)
+
 		
+class UserStatsView(APIView):
+	authentication_classes = (JSONWebTokenAuthentication, )
+	permission_classes = ()	
+	#IsAuthenticated,
+	def get(self, request, user_id=None, *args, **kw):
+		#timers = TimeRecord.objects.filter(owner=request.user.id)
+		status_arg = request.GET.get('status', None)
+		if status_arg:
+			timers = TimeRecord.objects.filter(owner=user_id,status=status_arg)
+		else:
+			timers = TimeRecord.objects.filter(owner=user_id)
+		# Add types condition
+		serializer = TimeRecordSerializer(timers,many=True)
+		return Response(serializer.data, status=status.HTTP_200_OK)
 
 class RegisterView(APIView):
 	authentication_classes = (JSONWebTokenAuthentication, )
@@ -46,28 +90,3 @@ class RegisterView(APIView):
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		else:
 			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-   
-class TimerView(APIView):
-	authentication_classes = (JSONWebTokenAuthentication, )
-	permission_classes = (IsAuthenticated,)
-
-	def get(self, request, format=None):
-		timers = TimeRecord.objects.filter(owner=request.user.id)
-		serializer = TimeRecordSerializer(timers,many=True)
-		return Response(serializer.data, status=status.HTTP_200_OK)
-	
-	def post(self, request, format=None):
-		record = TimeRecordSerializer(data = request.data)
-		if record.is_valid():
-			record.save()
-			return Response(record.data, status=status.HTTP_201_CREATED)
-		return Response(record.errors, status=status.HTTP_400_BAD_REQUEST)
-		
-class UserStatsView(APIView):
-	authentication_classes = (JSONWebTokenAuthentication, )
-	permission_classes = (IsAuthenticated,)	
-
-	def get(self, request, format=None):
-		timers = TimeRecord.objects.filter(owner=request.user.id)
-		serializer = TimeRecordSerializer(timers,many=True)
-		return Response(serializer.data, status=status.HTTP_200_OK)
--- webservice/urls.py
+++ webservice/urls.py
@@ -3,11 +3,15 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from timer import views
 
 urlpatterns = [
-	url(r'^$', views.UserView.as_view()),
-	url(r'^user/$', views.UserView.as_view()),
+	url(r'^$', views.TimeRecordView.as_view()),
+	url(r'^user/$', views.UserProfileView.as_view()),
+	url(r'^user/(?P<user_id>\w+|)/$', views.UserProfileView.as_view()),
 	url(r'^register/$', views.RegisterView.as_view()),
-	url(r'^timer/$', views.TimerView.as_view()),
-	url(r'^stats/$', views.UserStatsView.as_view()),
+	url(r'^timer/$', views.TimeRecordView.as_view()),
+	url(r'^timer/(?P<user_id>\w+)/$', views.TimeRecordView.as_view()),
+	url(r'^timer/(?P<timer_id>\w+)/$', views.TimeRecordView.as_view()),
+	url(r'^statics/$', views.UserStatsView.as_view()),
+	url(r'^statics/(?P<user_id>\w+)/$', views.UserStatsView.as_view()),
 	url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
 	url(r'^api-token-refresh/', 'rest_framework_jwt.views.refresh_jwt_token'),
 ]
