bca5565959988fe7f70946f1fd82e8ab523b4911
Peker Mert <pekermert@email.com>
2015-04-21 19:40:21 +0300

    new branch

--- timer/views.py
+++ timer/views.py
@@ -8,7 +8,6 @@ from timer.models import TimeRecord,User
 from timer.serializers import TimeRecordSerializer,UserSerializer
 from timer.utils import check_timer,remaining_time
 
-	
 class UserProfileView (APIView):
 	authentication_classes = (JSONWebTokenAuthentication, )
 	permission_classes = ()
