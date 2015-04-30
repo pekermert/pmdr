fe30799c07a321515385f4d799ba08861a8c49c5
Peker Mert <pekermert@email.com>
2015-04-30 22:03:17 +0300

    api root changed as api-v1

--- webservice/urls.py
+++ webservice/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^apiv1/', include('timer.urls')),
+    url(r'^api-v1/', include('timer.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
